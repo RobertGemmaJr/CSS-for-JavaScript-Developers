d446222d8ac57cd1f7fa9a2a9ef5d4ef30636dad
RobertGemmaJr <rgemma.jr@gmail.com>
2021-12-20 17:38:32 -0500

    feat: Add IconInput

--- Module 3 - Mini Component Library/src/components/IconInput/IconInput.js
+++ Module 3 - Mini Component Library/src/components/IconInput/IconInput.js
@@ -7,6 +7,12 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+/** From Tutorial:
+ *  https://github.com/css-for-js/mini-component-library/blob/solution/src/components/IconInput/IconInput.js
+ * 
+ *  
+ */
+
 const IconInput = ({
   label,
   icon,
@@ -14,7 +20,80 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  let Input, iconSize;
+  if(size === "small") {
+    Input = SmallInput;
+    iconSize = 16;
+  } else if(size==="large") {
+    Input = LargeInput;
+    iconSize = 24;
+  } else throw new Error(`Invalid IconInput size: ${size}`)
+
+  return (
+    <Wrapper >
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <StyledIcon id={icon} size={iconSize} />
+      <Input
+        name="iconInput"
+        type="text"
+        placeholder={placeholder}
+        size={size}
+        width={width}
+        iconSize={iconSize}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.form`
+  position: relative;
+  display: black;
+  color: ${COLORS.gray700};
+  border-bottom: 2px solid ${COLORS.black};
+  width: max-content;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline: 2px solid ${props => props.focusColor};
+  }
+`
+
+const BaseInput = styled.input`
+  color: inherit;
+  background-color: inherit;
+  outline: none;
+  border: none;
+  width: ${props => props.width}px;
+  font-size: ${props => props.size === "small" ? 0.875 : 1}rem;
+  font-weight: 700; 
+
+  &::placeholder {
+    color: inherit;
+    font-weight: initial;
+  }
+`
+
+const SmallInput = styled(BaseInput)`
+  height: 24px;
+  font-size: 14px;
+  padding: 4px 4px 4px 24px;
+`;
+
+const LargeInput = styled(BaseInput)`
+  height: 36px;
+  font-size: 18px;
+  padding: 8px 8px 8px 36px;
+`;
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  margin: auto;
+`;
 
 export default IconInput;
