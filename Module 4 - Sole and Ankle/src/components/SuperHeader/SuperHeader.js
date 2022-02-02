1714719c9525671e7eb5e8ff70c1dd0c8add6b3a
RobertGemmaJr <rgemma.jr@gmail.com>
2022-02-02 14:16:47 -0500

    feat: style SuperHeader

--- Module 4 - Sole and Ankle/src/components/SuperHeader/SuperHeader.js
+++ Module 4 - Sole and Ankle/src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,14 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  padding: 12px 32px;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  flex: 1;
 `;
 
 const HelpLink = styled.a`
@@ -40,6 +44,8 @@ const HelpLink = styled.a`
   &:not(:focus-visible) {
     outline: none;
   }
+
+  margin: 0px 28px;
 `;
 
 export default SuperHeader;
