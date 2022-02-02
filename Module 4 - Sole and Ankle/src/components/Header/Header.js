1a42cd0f07156db05bc5ffddc1da43dd515df869
RobertGemmaJr <rgemma.jr@gmail.com>
2022-02-02 16:05:00 -0500

    add margin to Header

--- Module 4 - Sole and Ankle/src/components/Header/Header.js
+++ Module 4 - Sole and Ankle/src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
+        <Spacer>
         <Logo />
+        </Spacer>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,26 +23,31 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Spacer />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  /* padding: 0 32px; */ // From starter code
   border-bottom: 1px solid ${COLORS.gray[300]};
-  position: relative;
   display: flex;
-  justify-content: center;
   align-items: baseline;
-  /* height: 72px; */
+  padding: 22px 32px;
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
-  margin: 36px auto;
+  margin: 0 48px;
 `;
+
+// Spacer grows left and right equally
+// Even with <Logo /> inside the left side
+const Spacer = styled.span`
+  flex: 1;
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
